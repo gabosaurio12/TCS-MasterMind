7eb6fa624b146d5776c97d59bb876d303b2c0e15
GaboSaurio <gabgonlo05@gmail.com>
2026-09-15 23:23:34 -0600

    feat: wpf client -> SignupPage and LoginPage, .resx for en-US and es-MX

---
+++ src/MasterMind_Client/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MasterMind_Client
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        App()
+        {
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
+        }
+    }
+}
---
+++ src/MasterMind_Client/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MasterMind_Client
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : NavigationWindow
+    {
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+
+            this.WindowStyle = WindowStyle.None;
+            this.WindowState = WindowState.Maximized;
+        }
+
+        private void NavigationWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Navigated += MainWindow_Navigated;
+        }
+
+        private void MainWindow_Navigated(object sender, NavigationEventArgs e)
+        {
+            if (e.Content is Page page && !(page.Content is Viewbox))
+            {
+                var originalContent = page.Content;
+                page.Content = null;
+
+                var viewbox = new Viewbox { Stretch = Stretch.Uniform };
+                var container = new Grid { Width = 1920, Height = 1080 };
+
+                container.Children.Add((UIElement)originalContent);
+                viewbox.Child = container;
+                page.Content = viewbox;
+            }
+        }
+    }
+}
