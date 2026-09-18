213d33599534978b995a20948b0a8bc03d31403d
GaboSaurio <gabgonlo05@gmail.com>
2026-09-17 22:22:07 -0600

    feat: verification code temp auth and verification code modal

---
+++ src/MasterMind_Client/Modals/VerificationCodeModal.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Navigation;
+
+namespace MasterMind_Client.Modals
+{
+    /// <summary>
+    /// Lógica de interacción para VerificationCodeModal.xaml
+    /// </summary>
+    public partial class VerificationCodeModal : Window
+    {
+        private readonly string username;
+        private readonly NavigationService navigationService;
+
+        public VerificationCodeModal()
+        {
+            InitializeComponent();
+        }
+
+        public VerificationCodeModal(string username, NavigationService navigationService)
+        {
+            InitializeComponent();
+            this.username = username;
+            this.navigationService = navigationService;
+        }
+
+        private void MoveFocusToNext(TextBox currentTxt)
+        {
+            var request = new TraversalRequest(FocusNavigationDirection.Next);
+            currentTxt.MoveFocus(request);
+        }
+
+        private string GetCodeFromBoxes()
+        {
+            return string.Concat(
+                CodeI0Txt.Text,
+                CodeI1Txt.Text,
+                CodeI2Txt.Text,
+                CodeI3Txt.Text,
+                CodeI4Txt.Text
+            );
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Windows.OfType<VerificationCodeModal>().FirstOrDefault()?.Close();
+        }
+
+        private void ContinueBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string code = GetCodeFromBoxes();
+            var result = TempData.TempAuthService.AuthVerificationCode(username, code);
+            if (result)
+            {
+                navigationService.Navigate(new Uri("Pages/MainPage.xaml", UriKind.Relative));
+                Application.Current.Windows.OfType<VerificationCodeModal>().FirstOrDefault()?.Close();
+            }
+            else
+            {
+                new ErrorNotificationModal(Properties.Resources.ErrorNotification_WrongCode).Show();
+            }
+        }
+
+        private void CodeTxt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var currentTxt = (TextBox)sender;
+
+            if (currentTxt.Text.Length == 1)
+            {
+                MoveFocusToNext(currentTxt);
+            }
+        }
+
+        private void CodeTxt_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            var currentBox = (TextBox)sender;
+
+            if (e.Key == Key.Back && string.IsNullOrEmpty(currentBox.Text))
+            {
+                var request = new TraversalRequest(FocusNavigationDirection.Previous);
+                currentBox.MoveFocus(request);
+            }
+        }
+    }
+}
--- src/MasterMind_Client/TempData/TempAuthService.cs
+++ src/MasterMind_Client/TempData/TempAuthService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace MasterMind_Client.TempData
 {
@@ -12,9 +13,9 @@ namespace MasterMind_Client.TempData
     {
 
         public static List<TempPlayer> Players { get; set; } = new List<TempPlayer>();
-        private static int playersId = 1;
+        private static int playersId = 0;
         public static List<TempVerificationCode> VerificationCodes { get; set; } = new List<TempVerificationCode>();
-        private static int codesId = 1;
+        private static int codesId = 0;
         private static readonly Random Rand = new Random();
 
         public static bool AuthPlayer(TempPlayer player)
@@ -45,18 +46,47 @@ namespace MasterMind_Client.TempData
             Players.Add(player);
 
             SendVerificationCode(player.Id);
-
-            Console.WriteLine(player.Id);
             
             return RegistrationResult.Success;
         }
 
+        public static RegistrationResult MockRegisterPlayer(TempPlayer player)
+        {
+            if (Players.Any(p => p.Username == player.Username))
+            {
+                return RegistrationResult.UsernameTaken;
+            }
+            if (Players.Any(p => p.Email == player.Email))
+            {
+                return RegistrationResult.EmailTaken;
+            }
+
+            player.Id = ++playersId;
+            Players.Add(player);
+
+            return RegistrationResult.Success;
+        }
+
+        public static bool AuthVerificationCode(string username, string code)
+        {
+            var player = Players.FirstOrDefault(p => p.Username == username);
+            if (player != null)
+            {
+                var authCode = VerificationCodes.FirstOrDefault(vc => vc.Code == code && vc.PlayerId == player.Id);
+                if (authCode != null)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
         private static void SendVerificationCode(int playerId)
         {
-            var result = CreateVerificationCode(playersId);
+            var result = CreateVerificationCode(playerId);
             while (!result)
             {
-                CreateVerificationCode(playersId);
+                CreateVerificationCode(playerId);
             }
         }
 
@@ -65,7 +95,7 @@ namespace MasterMind_Client.TempData
             string code = "";
             for (int i = 0; i < 5; i++)
             {
-                code += Rand.Next(0,9).ToString();
+                code += Rand.Next(0,10).ToString();
             }
 
             if (VerificationCodes.FirstOrDefault(vc => vc.Code.Equals(code)) == null)
@@ -73,14 +103,15 @@ namespace MasterMind_Client.TempData
                 var currentCode = VerificationCodes.FirstOrDefault(vc => vc.PlayerId == playerId);
                 VerificationCodes.Remove(currentCode);
                 VerificationCodes.Add(
-                new TempVerificationCode
-                {
-                    Id = ++codesId,
-                    Code = code,
-                    PlayerId = playerId
-                });
+                    new TempVerificationCode
+                    {
+                        Id = ++codesId,
+                        Code = code,
+                        PlayerId = playerId
+                    });
 
-                Console.WriteLine("CODIGO ACTUAL: " + code);
+
+                MessageBox.Show("Código actual: " + code);
 
                 return true;
             }
