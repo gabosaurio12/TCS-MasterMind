bbbb7913b28a7a1a3d63a6487bd81f8b0a79f8de
GaboSaurio <gabgonlo05@gmail.com>
2026-09-20 00:09:17 -0600

    feat: temp FriendshipService, PlayerService with enums for errors and logger

--- src/MasterMind_Client/App.xaml.cs
+++ src/MasterMind_Client/App.xaml.cs
@@ -1,6 +1,12 @@
-﻿using System.Windows;
-using System.Globalization;
+﻿using log4net;
+using log4net.Config;
 using MasterMind_Client.TempData;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Xml.Serialization;
 
 namespace MasterMind_Client
 {
@@ -9,6 +15,9 @@ namespace MasterMind_Client
     /// </summary>
     public partial class App : Application
     {
+
+        private static readonly ILog log = LogManager.GetLogger(typeof(App));
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
@@ -18,21 +27,51 @@ namespace MasterMind_Client
             System.Threading.Thread.CurrentThread.CurrentUICulture = cultureToUse;
             CultureInfo.DefaultThreadCurrentUICulture = cultureToUse;
 
-            TempAuthService.MockRegisterPlayer(
-                new TempPlayer
-                {
-                    Username = "testuser",
-                    Password = "password123",
-                    Email = "testuser@example.com"
-                });
+            XmlConfigurator.Configure();
 
-            TempAuthService.MockRegisterPlayer(
-                new TempPlayer
-                {
-                    Username = "johndoe",
-                    Password = "securepass",
-                    Email = "johndoe@example.com"
-                });
+            log.Info("Starting seeding in the sandbox");
+
+            var testuser = new TempPlayer
+            {
+                Username = "testuser",
+                Password = "password123",
+                Email = "testuser@example.com",
+                Avatar = new BitmapImage(new Uri("Images/Avatars/Link.jpg", UriKind.Relative))
+            };
+
+            var johndoe = new TempPlayer
+            {
+                Username = "johndoe",
+                Password = "securepass",
+                Email = "johndoe@example.com",
+                Avatar = new BitmapImage(new Uri("Images/Avatars/MasterChief.jpg", UriKind.Relative))
+            };
+
+            var gabosaurio = new TempPlayer
+            {
+                Username = "gabosaurio",
+                Password = "gabo",
+                Email = "gabosaurio@gmail.com",
+                Avatar = new BitmapImage(new Uri("Images/Avatars/Patronus HD.jpg", UriKind.Relative))
+            };
+
+            var noobmaster = new TempPlayer
+            {
+                Username = "noobmaster69",
+                Password = "noob",
+                Email = "noobmaster@gmail.com",
+                Avatar = new BitmapImage(new Uri("Images/Avatars/Link con fondo.jpg", UriKind.Relative))
+            };
+
+            TempAuthService.MockRegisterPlayer(gabosaurio);
+            TempAuthService.MockRegisterPlayer(testuser);
+            TempAuthService.MockRegisterPlayer(johndoe);
+            TempAuthService.MockRegisterPlayer(noobmaster);
+
+            FriendshipService.SendFrienshipRequest(testuser.Id, gabosaurio.Id);
+            FriendshipService.SendFrienshipRequest(johndoe.Id, gabosaurio.Id);
+
+            log.Info("Seeding finished");
         }
 
         private CultureInfo DetermineStartupCulture()
---
+++ src/MasterMind_Client/TempData/TempFriendship.cs
@@ -0,0 +1,17 @@
+﻿using MasterMind_Client.TempData.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterMind_Client.TempData
+{
+    public class TempFriendship
+    {
+        public int Id { get; set; }
+        public int RequesterId { get; set; }
+        public int AddreseeId { get; set; }
+        public RequestStatusEnum Status { get; set; }
+    }
+}
